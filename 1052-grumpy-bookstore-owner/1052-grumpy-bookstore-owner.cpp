3200d0a814d048a6e698c92ffe52614a3cdc6863
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-05-11 23:00:18 +0530

    Time: 4 ms (17.66%), Space: 35.8 MB (38.74%) - LeetHub

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maxSatisfied(vector<int>& customers, vector<int>& grumpy, int minutes) {
+
+        int n = customers.size();
+
+        int satisfied = 0;
+
+        // Already satisfied customers
+        for(int i = 0; i < n; i++) {
+            if(grumpy[i] == 0) {
+                satisfied += customers[i];
+            }
+        }
+
+        // First window extra customers
+        int extra = 0;
+
+        for(int i = 0; i < minutes; i++) {
+            if(grumpy[i] == 1) {
+                extra += customers[i];
+            }
+        }
+
+        int maxExtra = extra;
+
+        // Sliding window
+        for(int i = minutes; i < n; i++) {
+
+            if(grumpy[i - minutes] == 1) {
+                extra -= customers[i - minutes];
+            }
+
+            if(grumpy[i] == 1) {
+                extra += customers[i];
+            }
+
+            maxExtra = max(maxExtra, extra);
+        }
+
+        return satisfied + maxExtra;
+    }
+};
