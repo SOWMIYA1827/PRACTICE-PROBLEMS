7bce7c999c8106fb96a4d44a36cd86b76b28ab02
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-05-13 21:26:19 +0530

    Add files via upload

---
+++ maximumones.cpp
@@ -0,0 +1,33 @@
+//Given a binary array nums and an integer k, return the maximum number of 
+//consecutive 1's in the array if you can flip at most k 0's.
+//Example 1:
+
+//Input: nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
+//Output: 6
+//Explanation: [1,1,1,0,0,1,1,1,1,1,1]
+//Bolded numbers were flipped from 0 to 1. The longest subarray is underlined.
+
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+        int left = 0 ;
+        int maxzero = 0 ;
+        int maxlen = 0 ;
+
+        for(int right = 0 ; right < nums.size() ; right++ ){
+           if(nums[right] == 0){
+              maxzero++;
+           }
+
+           while(maxzero > k ){
+                if(nums[left] == 0){
+                    maxzero -- ;
+                }
+                left++;
+           }
+
+           maxlen = max(maxlen , right-left+1);
+        }
+        return maxlen;
+    }
+};
