4153d13d876f49f0db62396bd7b70fedac299296
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-06-19 11:41:03 +0530

    Time: 3 ms (14.88%), Space: 13.2 MB (38.65%) - LeetHub

---
+++ 1313-decompress-run-length-encoded-list/1313-decompress-run-length-encoded-list.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        
+    vector<int> ans;
+    for(int i=0 ; i< nums.size() ; i++){
+
+        if(i%2 != 0){
+            continue;
+        }
+
+        for(int j=0 ; j<nums[i] ; j++){
+            ans.push_back(nums[i+1]);
+        }
+    }
+
+    return ans ;
+
+
+
+    }
+};
