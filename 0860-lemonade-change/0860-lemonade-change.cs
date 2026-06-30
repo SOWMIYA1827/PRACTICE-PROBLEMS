218d731e5940e872020428a219c97017357e7fa3
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-06-30 19:42:35 +0530

    Time: 1 ms (100%), Space: 56 MB (46.15%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public bool LemonadeChange(int[] bills) {
+        int five = 0 ;
+        int ten = 0 ;
+
+        foreach(int x in bills){
+            if(x == 5){
+                five++;
+            }else if ( x == 10){
+                if(five == 0){
+                    return false;
+                }
+                five--;
+                ten++;
+            }else{
+                if(five > 0 && ten > 0){
+                    five-- ; ten-- ;
+                }else if (five >= 3){
+                    five -= 3 ;
+                }else{
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
