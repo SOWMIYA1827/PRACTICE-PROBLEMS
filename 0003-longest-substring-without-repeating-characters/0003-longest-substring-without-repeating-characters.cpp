efa1bdbcc6550a4cb567169c851930c913bc4c12
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-08-07 10:01:29 +0530

    Time: 307 ms (7.37%), Space: 81.3 MB (7.73%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> st;
-        int left = 0;
+        unordered_set<char> st ;
+
+        int j = 0 ;
         int maxlen = 0 ;
+        for( int i=0 ; i<s.length() ; i++){
+           
+           while( st.find( s[i] ) != st.end() ){
+                st.erase(s[j]);
+                j++;
+           }
 
-        for(int right=0 ; right < s.length() ; right++){
-            
-            while ( st.count(s[right])){
-                st.erase(s[left]);
-                left++;
-            }
-
-            st.insert(s[right]);
-            maxlen = max ( maxlen , right-left+1);
+           st.insert( s[i] );
+           maxlen = max ( maxlen , i-j+1);
         }
-
-        return maxlen;
+        return maxlen ;
     }
 };
