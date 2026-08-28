3a400093e80f43f60f30dd23b042f4e98274d244
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-08-28 08:54:46 +0530

    Time: 0 ms (100%), Space: 12.6 MB (77.29%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,14 +1,19 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        
-        for(int i=0 ; i<nums.size() ; i++){
+        int n = nums.size();
 
-            if( (i==0 || nums[i] >nums[i-1]) && ( i==nums.size()-1 || nums[i] > nums[i+1])){
-                return i ;
+        int low = 0 , high = n-1 ;
+        while( low < high ){
+            int mid = low + ( high - low )/2 ;
+
+            if( nums[mid] < nums[mid+1]){
+                low = mid + 1 ;
+            }else{
+                high = mid ;
             }
         }
 
-        return -1;
+        return low ;
     }
 };
