fb800c55ac0728239e60ab903ac53c059d0a165b
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-01 21:10:55 +0530

    Time: 0 ms (100%), Space: 17.7 MB (87.28%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) {
-
-        int n = nums.size();
-        int prefix = 1 , suffix = 1 ;
-        int ans = INT_MIN ;
-
-        for(int i=0 ; i<n ; i++){
-
-            if( prefix == 0) {
-                prefix = 1 ;
-            }
-            if( suffix == 0) {
-                suffix = 1 ;
-            }
-            prefix = nums[i] * prefix ;
-            suffix = nums[n-i-1] * suffix ;
-
-            ans = max( ans , max(prefix , suffix));
-        }
-        return ans ;
+    int maxProduct(vector<int>& arr) {
+        int n = arr.size();
+    int prefix = 1 ;
+     int suffix = 1 ;
+     int ans = INT_MIN ;
+     
+     for(int i=0 ; i<n ; i++){
+         if( prefix == 0){
+             prefix = 1 ;
+         }
+         if( suffix == 0){
+             suffix = 1 ;
+         }
+         
+         prefix = prefix * arr[i];
+         suffix = suffix * arr[n-i-1];
+         
+         ans = max(ans , max(prefix , suffix));
+     }
+     
+     return ans ;
     }
 };
