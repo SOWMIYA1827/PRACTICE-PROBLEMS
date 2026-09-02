128eb6e2f44a302a855c91a4f0ebbe0af56a94a2
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-02 14:10:17 +0530

    Time: 0 ms (100%), Space: 14.5 MB (74.54%) - LeetHub

--- 0089-gray-code/0089-gray-code.cpp
+++ 0089-gray-code/0089-gray-code.cpp
@@ -3,9 +3,17 @@ public:
     vector<int> grayCode(int n) {
         vector<int> result ;
 
-        int numberofelements = 1 << n ;
+        // int numberofelements = 1 << n ;
 
-        for(int i=0 ; i<numberofelements ; i++){
+        // for(int i=0 ; i<numberofelements ; i++){
+        //     int gray = i ^ (i>>1);
+        //     result.push_back(gray);
+        // }
+
+        // return result ;
+
+        int num = 1 << n ;
+        for(int i=0 ; i<num ; i++){
             int gray = i ^ (i>>1);
             result.push_back(gray);
         }
