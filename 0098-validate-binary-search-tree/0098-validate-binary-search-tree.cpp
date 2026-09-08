c1a2ef988635a3415987e0570278e9106eb4c7c5
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-08 10:05:17 +0530

    Time: 0 ms (100%), Space: 22 MB (44.6%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -1,30 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
-private:
-    bool inorder(TreeNode* root, TreeNode*& prev) {
-        if (root == nullptr) {
-            return true;
-        }
-        
-        // Check the left subtree
-        if (!inorder(root->left, prev)) {
-            return false;
-        }
-        
-        // Validate current node's value against the previous node
-        if (prev != nullptr && root->val <= prev->val) {
-            return false;
-        }
-        
-        // Update the previous node pointer to the current node
-        prev = root;
-        
-        // Check the right subtree
-        return inorder(root->right, prev);
-    }
-
 public:
+    bool validate( TreeNode* root , long long low , long long high){
+
+        if( root == NULL ){
+            return true ;
+        }
+
+        if( root->val <= low || root->val >= high ){
+            return false ;
+        }
+
+        return validate( root->left , low , root->val ) &&
+                     validate( root->right , root->val , high );
+    }
     bool isValidBST(TreeNode* root) {
-        TreeNode* prev = nullptr;
-        return inorder(root, prev);
+        return validate( root , LLONG_MIN , LLONG_MAX );
     }
 };
