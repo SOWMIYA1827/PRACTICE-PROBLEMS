f961dd925a6d77c445482d29494e5754e448db62
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-09 13:51:30 +0530

    Time: 0 ms (100%), Space: 10.1 MB (79.59%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -6,21 +6,20 @@ public:
         if( n<0 ){
             return 0 ;
         }
-
         if( n==0 ){
-            return nums[0];
+            return nums[0] ;
         }
 
         if( dp[n] != -1){
             return dp[n];
         }
 
-        return dp[n] = max( solve(nums , n-1) , nums[n] + solve(nums , n-2));
+        dp[n] = max (solve(nums , n-1) , nums[n] + solve(nums , n-2));
+        return dp[n];
     }
     int rob(vector<int>& nums) {
         fill( dp , dp+101 , -1);
-        int n = nums.size()-1;
-        return solve(nums,n);
+        int n = nums.size() - 1;
+        return solve(nums , n);
     }
-
 };
