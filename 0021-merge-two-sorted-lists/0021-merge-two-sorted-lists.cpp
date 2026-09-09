be651eec4f0d93ec677655ed0ac29d1f79d8365d
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-09 15:37:07 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.57%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,34 +8,26 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode dummy(0);
-        ListNode* temp = &dummy ;
+        ListNode dummy(0) ;
+        ListNode* tail = &dummy ;
 
-        while( list1 != nullptr && list2 != nullptr ){
-            if( list1 -> val <= list2 -> val ){
-                temp->next = list1 ;
-                list1 = list1 -> next;
+        while ( list1 && list2 ){
+            if( list1->val <= list2->val ){
+                tail->next = list1 ;
+                list1= list1->next ;
             }
             else{
-                temp->next = list2 ;
-                list2 = list2 -> next ;
+                tail->next = list2 ;
+                list2 = list2->next ;
             }
 
-            temp = temp->next;
+            tail = tail->next ;
         }
 
-        if( list1 != nullptr ){
-            temp->next = list1 ;
-        }
-
-        if( list2 != nullptr ){
-            temp->next = list2 ;
-        }
-
+        tail->next = (list1) ? list1 : list2 ;
         return dummy.next ;
     }
 };
