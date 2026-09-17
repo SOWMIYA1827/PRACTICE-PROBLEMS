a1d757387eb7b1fedc251a689c3ee96e328441a9
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-17 21:43:55 +0530

    Time: 7 ms (37.78%), Space: 30 MB (11.01%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool ispalin(string s){
+        int l = 0 , r = s.length()-1 ;
+
+        while( l<r){
+            if( s[l] != s[r]){
+                return false ;
+            }
+            l++ , r-- ;
+        }
+
+        return true ;
+    }
+    string firstPalindrome(vector<string>& words) {
+        
+        for(string s : words){
+            if( ispalin(s)){
+                return s ;
+            }
+        }
+
+        return "";
+    }
+};
