e415477ba35963a1037c61a15935368959f4e712
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-19 14:33:46 +0530

    Time: 0 ms (100%), Space: 23 MB (37.81%) - LeetHub

--- 3024-type-of-triangle/3024-type-of-triangle.cpp
+++ 3024-type-of-triangle/3024-type-of-triangle.cpp
@@ -4,19 +4,16 @@ public:
         int a = nums[0];
         int b = nums[1];
         int c = nums[2];
-        if (a+b > c && a+c > b && c+b > a){
-         
-         if( a==b && b==c && c==a){
-            return "equilateral";
+        if (a + b > c && a + c > b && c + b > a) {
+
+            if (a == b && b == c && c == a) {
+                return "equilateral";
+            } else if (a != b && b != c && c != a) {
+                return "scalene";
+            } else {
+                return "isosceles";
+            }
         }
-        else if ( a!=b && b!=c && c!=a){
-            return "scalene";
-        }
-        else {
-            return "isosceles";
-        }
-        }
-        
 
         return "none";
     }
