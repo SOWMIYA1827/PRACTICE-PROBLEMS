876010417cc941c538ca12212ccfeeef7834b7fc
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-22 22:15:09 +0530

    Time: 2 ms (57.33%), Space: 7.7 MB (99.75%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.cpp
+++ 0278-first-bad-version/0278-first-bad-version.cpp
@@ -5,18 +5,18 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int low = 1;
-        int high = n ;
+        int high = n;
 
-        while( low < high ){
-            int mid = low + (high - low)/2 ;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
 
-            if(isBadVersion(mid)){
-                high = mid ;
-            }else{
-                low = mid + 1 ;
+            if (isBadVersion(mid)) {
+                high = mid;
+            } else {
+                low = mid + 1;
             }
         }
 
-        return low ;
+        return low;
     }
 };
