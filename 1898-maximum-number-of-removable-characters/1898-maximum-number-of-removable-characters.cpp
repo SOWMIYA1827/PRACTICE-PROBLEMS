84038c159958c8bd7842ea26dd8d4198cab96d3b
SOWMIYA MURUGAN <24csb34@karpagamtech.ac.in>
2026-09-25 22:54:43 +0530

    Time: 192 ms (25.94%), Space: 142.6 MB (30.07%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.cpp
@@ -1,50 +1,48 @@
 class Solution {
 public:
+    bool issequence(string temp, string p) {
+        int i = 0, j = 0;
 
-    bool issequence(string temp , string p){
-        int i=0 , j=0 ;
-
-        while( i<temp.length() && j<p.length()){
-            if( temp[i] == p[j]){
+        while (i < temp.length() && j < p.length()) {
+            if (temp[i] == p[j]) {
                 j++;
             }
             i++;
         }
-        return j == p.length() ;
+        return j == p.length();
     }
     int maximumRemovals(string s, string p, vector<int>& removable) {
         int n = removable.size();
 
-        int low = 0 , high = n ;
-        int ans = 0 ;
+        int low = 0, high = n;
+        int ans = 0;
 
-        while( low <= high){
-            int mid = low + (high - low)/2 ;
-           
-            vector<bool> removed(s.length() , false);
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            for(int i=0 ; i<mid ; i++){
-                removed[removable[i]] = true ;
+            vector<bool> removed(s.length(), false);
+
+            for (int i = 0; i < mid; i++) {
+                removed[removable[i]] = true;
             }
 
             string temp = "";
-            for(int i=0 ; i<s.length() ; i++){
-                if(!removed[i]){
+            for (int i = 0; i < s.length(); i++) {
+                if (!removed[i]) {
                     temp += s[i];
                 }
             }
 
-            bool isseq = issequence(temp , p);
+            bool isseq = issequence(temp, p);
 
-            if(isseq){
-                ans = mid ;
-                low = mid + 1 ;
-            }
-            else{
+            if (isseq) {
+                ans = mid;
+                low = mid + 1;
+            } else {
                 high = mid - 1;
             }
         }
 
-        return ans ;
+        return ans;
     }
 };
